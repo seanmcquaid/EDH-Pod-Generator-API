b9e54ca9d665a052224b95b3e720b63748d00cd7
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-17 15:45:12 -0400

    initial commit

---
+++ src/main/java/com/edh/pod/generator/api/EdhPodGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.edh.pod.generator.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdhPodGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdhPodGeneratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edh/pod/generator/api/EdhPodGeneratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edh.pod.generator.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdhPodGeneratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
