c3b0c0823180d8c69cf2b5eaa68110dc2ecbdd5a
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-31 20:07:51 -0400

    set up pattern for returning json

--- src/main/java/com/edh/pod/generator/api/controllers/UsersController.java
+++ src/main/java/com/edh/pod/generator/api/controllers/UsersController.java
@@ -3,12 +3,16 @@ package com.edh.pod.generator.api.controllers;
 import com.edh.pod.generator.api.models.User;
 import com.edh.pod.generator.api.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/users")
@@ -17,20 +21,26 @@ public class UsersController {
     @Autowired
     private UsersService usersService;
 
-    @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    public UsersController() {
+        this.bCryptPasswordEncoder = new BCryptPasswordEncoder();
+    }
+
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody User user){
+    public ResponseEntity<Map<String, String>> register(@RequestBody User user){
         boolean doesUserExist = usersService.doesUserExist(user.getUsername());
         if(doesUserExist){
-            return ResponseEntity.status(401).build();
+            Map<String, String> body = new HashMap<>();
+            body.put("errorMessage", "This username has already been created, please try another one");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
         }
 
         String encodedPassword = bCryptPasswordEncoder.encode(user.getPassword());
         User newUserInfo = usersService.addUser(user.getUsername(), encodedPassword);
 
 //        generate JWT and send in response
+
 
 
         return ResponseEntity.ok().build();
