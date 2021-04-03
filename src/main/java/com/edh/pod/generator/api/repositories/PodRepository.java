b70aec60b6441ce27e1238edabbd55278417c856
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-03 19:20:48 -0400

    fleshed out needed services

--- src/main/java/com/edh/pod/generator/api/repositories/PodRepository.java
+++ src/main/java/com/edh/pod/generator/api/repositories/PodRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 
 public interface PodRepository extends JpaRepository<Pod, Integer>{
 
-    @Query(value = "insert into pods(owner, member, member_email, spell_table_url, name) values (:owner, :member, :member_email, :spell_table_url, :name) returning *", nativeQuery = true)
-    List<Pod> addPodMember(@Param("owner") String owner, @Param("member") String member, @Param("member_email") String member_email, @Param("spell_table_url") String spell_table_url, @Param("name") String name);
+    @Query(value = "insert into pods(owner, member, member_email, spell_table_url, name) values (:owner, :member, :member_email, :spell_table_url, :name)", nativeQuery = true)
+    void addPodMember(@Param("owner") String owner, @Param("member") String member, @Param("member_email") String member_email, @Param("spell_table_url") String spell_table_url, @Param("name") String name);
 
     @Query(value = "select * from pods where owner = :owner", nativeQuery = true)
     List<Pod> getPods(@Param("owner") String owner);
--- src/main/java/com/edh/pod/generator/api/services/PodsService.java
+++ src/main/java/com/edh/pod/generator/api/services/PodsService.java
@@ -1,7 +1,24 @@
 package com.edh.pod.generator.api.services;
 
+import com.edh.pod.generator.api.models.Pod;
+import com.edh.pod.generator.api.repositories.PodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PodsService {
+
+    @Autowired
+    private PodRepository podRepository;
+
+    public List<Pod> addPodMember(Pod podInfo){
+        podRepository.addPodMember(podInfo.getOwner(), podInfo.getMember(), podInfo.getMemberEmail(), podInfo.getSpellTableUrl(), podInfo.getName());
+        return podRepository.getPod(podInfo.getOwner(), podInfo.getName());
+    }
+
+    public List<Pod> getPods(String owner, String name){
+        return podRepository.getPod(owner, name);
+    }
 }
