b2ca33f5ca0777b3537dc69f46fe3cd6af597477
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-04 19:31:45 -0400

    finished get tests

--- src/test/java/com/edh/pod/generator/api/controllers/PodsControllerTests.java
+++ src/test/java/com/edh/pod/generator/api/controllers/PodsControllerTests.java
@@ -1,15 +1,29 @@
 package com.edh.pod.generator.api.controllers;
 
+import com.edh.pod.generator.api.models.Pod;
 import com.edh.pod.generator.api.services.PodsService;
 import com.edh.pod.generator.api.services.UsersService;
 import com.edh.pod.generator.api.utils.TestUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.mockito.Mockito.when;
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(PodsController.class)
@@ -30,10 +44,63 @@ public class PodsControllerTests {
     }
 
     @Test
-    public void getPodsAuthNotValidTest(){}
+    public void getPodsAuthNotValidTest() throws Exception {
+        when(usersService.isTokenValid(any(String.class))).thenReturn(false);
+
+        mockMvc.perform(get("/pods")
+                .header("Authorization", "token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.errorMessage").value("The provided token isn't valid, please login again"));
+    }
 
     @Test
-    public void getPodsForUsernameAuthValidTest(){}
+    public void getPodsForUsernameAuthValidTest() throws Exception {
+        List<Pod> pods = new ArrayList<>();
+
+        Pod podOneMember = new Pod();
+        podOneMember.setId(1);
+        podOneMember.setOwner("sean");
+        podOneMember.setMember("terrell");
+        podOneMember.setMemberEmail("memberemail@gmail.com");
+        podOneMember.setSpellTableUrl("spelltable.com");
+        podOneMember.setName("name1");
+
+        Pod podTwoMember = new Pod();
+        podTwoMember.setId(1);
+        podTwoMember.setOwner("sean");
+        podTwoMember.setMember("victor");
+        podTwoMember.setMemberEmail("memberemail@gmail.com");
+        podTwoMember.setSpellTableUrl("spelltable.com");
+        podTwoMember.setName("name2");
+
+        pods.add(podOneMember);
+        pods.add(podTwoMember);
+
+        List<Pod> podOne = new ArrayList<>();
+        podOne.add(pods.get(0));
+
+        List<Pod> podTwo = new ArrayList<>();
+        podTwo.add(pods.get(1));
+
+        List<List<Pod>> expectedResults = new ArrayList<>();
+        expectedResults.add(podOne);
+        expectedResults.add(podTwo);
+
+        String encodedToken = testUtils.generateToken("sean");
+        Jws<Claims> decodedToken = testUtils.decodeToken(encodedToken);
+
+        when(usersService.isTokenValid(any(String.class))).thenReturn(true);
+        when(usersService.decodeToken(any())).thenReturn(decodedToken);
+        when(podsService.getPods(any(String.class))).thenReturn(pods);
+        when(podsService.sortIntoPods(any())).thenReturn(expectedResults);
+
+        mockMvc.perform(get("/pods")
+                .header("Authorization", "token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.pods").isArray());
+    }
 
     @Test
     public void addPodMemberAuthNotValidTest(){}
