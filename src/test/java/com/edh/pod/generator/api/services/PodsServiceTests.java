97c50ffea9c3c4c4a3477f3100f2aae17c2e8247
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-12 19:54:22 -0400

    finished sort into pods test

--- src/test/java/com/edh/pod/generator/api/services/PodsServiceTests.java
+++ src/test/java/com/edh/pod/generator/api/services/PodsServiceTests.java
@@ -1,8 +1,15 @@
 package com.edh.pod.generator.api.services;
 
+import com.edh.pod.generator.api.models.PodMember;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 
 @RunWith(SpringRunner.class)
 public class PodsServiceTests {
@@ -13,7 +20,28 @@ public class PodsServiceTests {
     }
 
     @Test
-    public void sortIntoPodsTest(){}
+    public void sortIntoPodsTest(){
+        List<PodMember> podMembers = new ArrayList<>();
+
+        PodMember podMemberOneMember = new PodMember();
+        podMemberOneMember.setId(1);
+        podMemberOneMember.setOwner("sean");
+        podMemberOneMember.setMember("terrell");
+        podMemberOneMember.setMemberEmail("memberemail@gmail.com");
+        podMemberOneMember.setName("name1");
+
+        PodMember podMemberTwoMember = new PodMember();
+        podMemberTwoMember.setId(1);
+        podMemberTwoMember.setOwner("sean");
+        podMemberTwoMember.setMember("victor");
+        podMemberTwoMember.setMemberEmail("memberemail@gmail.com");
+        podMemberTwoMember.setName("name2");
+
+        podMembers.add(podMemberOneMember);
+        podMembers.add(podMemberTwoMember);
+
+        assertEquals(podsService.sortIntoPods(podMembers).size(), 2);
+    }
 
     @Test
     public void getPodByNameTest(){}
