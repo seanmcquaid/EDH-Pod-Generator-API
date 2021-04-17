88863cd4da4b99983e937cfd0a2b26c9dc285ab8
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-17 12:16:53 -0400

    finished all unit tests

--- src/test/java/com/edh/pod/generator/api/controllers/PodsControllerTests.java
+++ src/test/java/com/edh/pod/generator/api/controllers/PodsControllerTests.java
@@ -236,7 +236,7 @@ public class PodsControllerTests {
     }
 
     @Test
-    public void deletePodMemberAuthValidTest(){
+    public void deletePodMemberAuthValidTest() throws Exception {
         PodMember podMember = new PodMember();
         podMember.setId(1);
         podMember.setOwner("sean");
@@ -249,5 +249,19 @@ public class PodsControllerTests {
 
         List<Pod> sortedPods = new ArrayList<>();
         sortedPods.add(new Pod(podMembers, "name1"));
+
+        String encodedToken = testUtils.generateToken("sean");
+        Jws<Claims> decodedToken = testUtils.decodeToken(encodedToken);
+
+        when(usersService.isTokenValid(any(String.class))).thenReturn(true);
+        when(usersService.decodeToken(any())).thenReturn(decodedToken);
+        when(podsService.deletePodMember(any(), any(), any())).thenReturn(podMembers);
+        when(podsService.sortIntoPods(any())).thenReturn(sortedPods);
+
+        mockMvc.perform(delete("/pods/name1/member/sean")
+                .header("Authorization", "token")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+                .andExpect(jsonPath("$.pods").isArray());
     }
 }
